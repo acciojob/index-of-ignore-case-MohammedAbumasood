0ad47dab1e69b1d49397879bb2ca01fe6c4c271b
MohammedAbumasood <abumasood657@gmail.com>
2025-05-08 06:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(s1 === ""){
+		return 0;
+	} 
+	if(s2 === ""){
+		return -1;
+	}
+
+	const lowerStr = s1.toLowerCase();
+	const lowerSubStr = s2.toLowerCase();
+
+	return lowerStr.indexOf(lowerSubStr);
 }
 
-// Please do not change the code below
+//Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
